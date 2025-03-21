a2632275570d70a91142ae882031aefd99f1bf03
JS Martinez <jason.guerra253@gmail.com>
2025-03-20 23:06:13 -0600

    feat(backend) Se agregaron validaciones al backend

--- pruebatecnica/src/main/java/com/serfinsa/pruebatecnica/controller/ProductoController.java
+++ pruebatecnica/src/main/java/com/serfinsa/pruebatecnica/controller/ProductoController.java
@@ -13,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,7 +42,7 @@ public class ProductoController extends Controller{
     }
 
     @PostMapping(path = "", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> post(@RequestBody ProductoDto productoDto, Authentication authentication) {
+    public ResponseEntity<?> post(@Validated @RequestBody ProductoDto productoDto, Authentication authentication) {
         try {
             return ResponseEntity.ok(productService.createProducto(productoDto, authentication));
         } catch (Exception e) {
--- pruebatecnica/src/main/java/com/serfinsa/pruebatecnica/domain/dto/ProductoDto.java
+++ pruebatecnica/src/main/java/com/serfinsa/pruebatecnica/domain/dto/ProductoDto.java
@@ -1,5 +1,10 @@
 package com.serfinsa.pruebatecnica.domain.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,9 +15,18 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductoDto {
+    @NotBlank(message = "El nombre del producto es requerido")
+    @NotEmpty(message = "El nombre del producto no puede estar vacío")
+    @Size(max = 50, message = "El nombre del producto no puede tener más de 50 caracteres")
     private String nombre;
+    @NotBlank(message = "La descripción del producto es requerida")
+    @Size(max = 250, message = "La descripción del producto no puede tener más de 250 caracteres")
+    @NotEmpty(message = "La descripción del producto no puede estar vacía")
     private String descripcion;
+    @NotNull(message = "El precio del producto es requerido")
     private Double precio;
+    @NotNull(message = "El stock del producto es requerido")
     private Integer stock;
+    @NotNull(message = "El idTipoProducto del producto es requerido")
     private Integer idTipoProducto;
 }
